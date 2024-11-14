9fc255716796a6742f4f1023a82531bfe8ef1e8b
AmBro <axmbro@gmail.com>
2024-11-14 19:05:24 +0100

    minor changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -37,7 +37,7 @@ function Home() {
               }}
               to="/projects"
             >
-              Check Projects
+              My Projects
             </Link>
           </>
         }
